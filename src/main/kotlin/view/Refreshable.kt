fc4e17146474e54b2d918c207fa6943d0a63cab9
jason.ball <jason.ball@tu-dortmund.de>
2024-11-07 11:03:07 +0100

    Service + Tests

--- src/main/kotlin/view/Refreshable.kt
+++ src/main/kotlin/view/Refreshable.kt
@@ -1,19 +1,35 @@
 package view
 
 class Refreshable {
+    /**
+     */
     fun refreshAfterStartGame() {}
 
+    /**
+     */
     fun refreshAfterEndGame() {}
 
+    /**
+     */
     fun refreshAfterStartTurn() {}
 
+    /**
+     */
     fun refreshAfterEndTurn() {}
 
+    /**
+     */
     fun refreshAfterPlayCard() {}
 
+    /**
+     */
     fun refreshAfterDrawCard() {}
 
+    /**
+     */
     fun refreshAfterSwapCard() {}
 
+    /**
+     */
     fun refreshAfterDiscardCard() {}
 }
